0ba455f20dda10fcbf6d530ce9d26534d77fdb05
Jarod <lee.jarod@gmail.com>
2020-08-02 19:03:42 +0800

    2002版演示网校
    当时是本市的第一个网络学校的实验，可惜当时选错领导，白浪费了一番努力。最后也没有演示。

---
+++ 2002/GHT/KLSG/SNACK/mcdonalds/www.mcdonalds.com.cn/campaign/mcd_wcd.js
@@ -0,0 +1,11 @@
+
+function MM_openBrWindow(theURL,winName,features) { //v2.0
+  window.open(theURL,winName,features);
+}
+function do_login(){
+	window.open("campaign/login.asp","login_win","height=520,width=590");
+}
+function do_forward(banner_id){
+	var banner_id;
+	window.open("campaign/forward.asp","forward_win","height=540,width=615");
+}
